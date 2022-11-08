53c97974a5f372448f2f89abd6473cbe92d7c086
marcelemontalvao <mmontalvaoblog@outlook.com>
2022-11-08 15:31:10 -0300

    Questão do fechamento do balanço do primeiro trimestre

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int janeiro = 10000;
+        int fevereiro = 17000;
+        int marco = 23000;
+        Integer gasto = janeiro +  fevereiro + marco;
+        System.out.println("O gasto total do trimestre foi R$ " +  gasto + " reais");
+    }
+}
